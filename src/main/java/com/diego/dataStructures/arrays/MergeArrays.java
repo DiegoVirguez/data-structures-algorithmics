af97962b52076bd79105d6ea47b26457422ec9df
Diego Virguez <diego.virguez@xpo.com>
2020-11-17 09:43:15 -0500

    adding merge array

---
+++ src/main/java/com/diego/dataStructures/arrays/MergeArrays.java
@@ -0,0 +1,39 @@
+package com.diego.dataStructures.arrays;
+
+/*
+ * [0,3,4,31] - [4,6,30]
+ * */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MergeArrays {
+
+
+    public static void merge(){
+    int[] a = {0, 3, 4, 31};
+    int[] b = {4, 6, 30};
+
+    List<Integer> c = new ArrayList<>();
+
+        for (int i = 0; i < a.length; i++) {
+            c.add(a[i]);
+        }
+
+        for (int i = 0; i < b.length; i++) {
+            c.add(b[i]);
+        }
+
+        System.out.println(c);
+        Collections.sort(c);
+        System.out.println(c);
+
+    }
+
+    public static void main(String[] args) {
+        merge();
+    }
+
+}
